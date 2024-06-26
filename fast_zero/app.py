6eaee3bf9069df92b123c111f9cc69dd029bca91
RRFFTT <rl.rfftt@proton.me>
2024-06-26 00:57:21 -0300

    Aula 2 - Introdução ao desenvolvimento WEB
    Implementado o exercicio 02.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,31 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get(
+    path='/retornohtml', status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
+def exaula2():
+    return """
+    <!DOCTYPE html>
+    <html lang="pt-br">
+      <head>
+        <title>Aula 2 - Dunossauro FastApi</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Olá Mundo!<br>Exercicio aula 2!</h1>
+      </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,23 @@
 from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
 def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) #(A) ARRANGE (organização)
-    response = client.get('/') # (A) ACT (ação)
+    client = TestClient(app)  # (A) ARRANGE (organização)
+    response = client.get('/')  # (A) ACT (ação)
 
-    assert response.status_code == HTTPStatus.OK # (A) Garantir que esta funcionado
+    assert (
+        response.status_code == HTTPStatus.OK
+    )  # (A) Garantir que esta funcionado
     assert response.json() == {'message': 'Olá mundo!'}
-    ...
+
+
+def test_retornohtml_deve_retornar_ok_e_o_codigo_html():
+    client = TestClient(app)
+
+    response = client.get(url='/retornohtml')
+
+    assert response.status_code == HTTPStatus.OK
